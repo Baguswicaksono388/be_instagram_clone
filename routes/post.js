cb88f70599d68c4c733c1eb23cef1b5858e29667
Bagus <sgiapi1@gmail.com>
2021-05-08 14:55:25 +0700

    Bagus [Dev] Creating API comments

--- routes/post.js
+++ routes/post.js
@@ -75,6 +75,26 @@ router.put('/like',requireLogin,(req, res) => {
     })
 })
 
+router.put('/like',requireLogin,(req, res) => {
+    const comment = {
+        text:req.body.text,
+        postedBy:req.user._id //req.user didapatkan di middleware
+    }
+    Post.findByIdAndUpdate(req.body.postId, {
+        $push:{comments:comment}
+    }, {
+        new: true,
+    })
+    .populate("comments.postedBy","_id name")
+    .exec((err, result) => {
+        if(err) {
+            return res.status(422).json({error:err});
+        }else{
+            res.json(result);
+        }
+    })
+})
+
 router.put('/unlike',requireLogin,(req, res) => {
     Post.findByIdAndUpdate(req.body.postId, {
         $pull:{likes:req.user._id} //req.user didapatkan di middleware
